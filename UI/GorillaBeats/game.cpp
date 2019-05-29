7ffa348e697cecd3915490b5a1a9501e1673b3a5
Xia Yu <xiayu@KaixienoMacBook-Pro.local>
2019-05-29 15:46:21 +0900

    game.h

---
+++ UI/GorillaBeats/game.cpp
@@ -0,0 +1,41 @@
+//
+//  game.cpp
+//  GorillaBeats
+//
+//  Created by Xia Yu on 2019/05/29.
+//  Copyright © 2019 gb. All rights reserved.
+//
+
+#include "game.hpp"
+
+Game::Game(GLuint width, GLuint height)
+: State(GAME_ACTIVE), Keys(), Width(width), Height(height)
+{
+    
+}
+
+Game::~Game()
+{
+    
+}
+
+void Game::Init()
+{
+    
+}
+
+void Game::Update(GLfloat dt)
+{
+    
+}
+
+
+void Game::ProcessInput(GLfloat dt)
+{
+    
+}
+
+void Game::Render()
+{
+    
+}
---
+++ UI/GorillaBeats/game.hpp
@@ -0,0 +1,44 @@
+//
+//  game.hpp
+//  GorillaBeats
+//
+//  Created by Xia Yu on 2019/05/29.
+//  Copyright © 2019 gb. All rights reserved.
+//
+
+#ifndef game_hpp
+#define game_hpp
+
+#include <GLFW/glfw3.h>
+#include <OpenGL/gl3.h>
+#include <OpenGL/gl3ext.h>
+
+// Represents the current state of the game
+enum GameState {
+    GAME_ACTIVE,
+    GAME_MENU,
+    GAME_WIN
+};
+
+// Game holds all game-related state and functionality.
+// Combines all game-related data into a single class for
+// easy access to each of the components and manageability.
+class Game
+{
+public:
+    // Game state
+    GameState              State;
+    GLboolean              Keys[1024];
+    GLuint                 Width, Height;
+    // Constructor/Destructor
+    Game(GLuint width, GLuint height);
+    ~Game();
+    // Initialize game state (load all shaders/textures/levels)
+    void Init();
+    // GameLoop
+    void ProcessInput(GLfloat dt);
+    void Update(GLfloat dt);
+    void Render();
+};
+
+#endif /* game_hpp */
